eca00e697d8bdd908c56c34280be5aa2a379580f
Joana Nahorny <97913270+JoanaNahorny@users.noreply.github.com>
2022-07-18 20:55:44 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		
+		//convenção Java
+		int idade = 20;
+		String nome = "Loiane";
+		String nomeDoMeuCachorro = "Totó";
+		
+		idade = 25;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		
+	}
+
+}
